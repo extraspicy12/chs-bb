9fb7f017c03b85cc484c987022ffe00f4a7618d0
MattK26 <MattKeimig26@gmail.com>
2013-04-08 08:03:54 -0400

    Semi-working bar angles

--- Bar.java
+++ Bar.java
@@ -22,47 +22,40 @@ public class Bar extends Component{
 		y = height;
 	}	
 	public void move(int newX){
-		if((newX==screenSize.width)||(newX==0)){
-			dx=0;
-			y=0;
+		convertedDX = (double)(newX-x)/(double)screenSize.width;
+		dx = (newX-x)/10;
+		if(convertedDX <= mark10){
+			angle = 90;
+		}else if(convertedDX < mark9){
+			angle = 82;
+		}else if(convertedDX < mark8){
+			angle = 74;
+		}else if(convertedDX < mark7){
+			angle = 66;
+		}else if(convertedDX < mark5){
+			angle = 58;
+		}else if(convertedDX < mark5){
+			angle = 50;
+		}else if(convertedDX < mark4){
+			angle = 42;
+		}else if(convertedDX < mark3){
+			angle = 34;
+		}else if(convertedDX < mark2){
+			angle = 26;
+		}else if(convertedDX < mark1){
+			angle = 18;
+		}else if(convertedDX < mark05){
+			angle = 10;
+		}else{
 			angle = 0;
 		}
-		else{
-//			convertedDX = (double)(newX-x)/(double)screenSize.width;
-//			dx = (newX-x)/10;
-//			if(convertedDX <= mark10){
-//				angle = 90;
-//			}else if(convertedDX < mark9){
-//				angle = 82;
-//			}else if(convertedDX < mark8){
-//				angle = 74;
-//			}else if(convertedDX < mark7){
-//				angle = 66;
-//			}else if(convertedDX < mark5){
-//				angle = 58;
-//			}else if(convertedDX < mark5){
-//				angle = 50;
-//			}else if(convertedDX < mark4){
-//				angle = 42;
-//			}else if(convertedDX < mark3){
-//				angle = 34;
-//			}else if(convertedDX < mark2){
-//				angle = 26;
-//			}else if(convertedDX < mark1){
-//				angle = 18;
-//			}else if(convertedDX < mark05){
-//				angle = 10;
-//			}else{
-//				angle = 2;
-//			}
-			x+=dx;
-		}
+		x+=dx;
+
 	}
 	public void paint(Graphics block){
-		block.setColor(Color.BLACK);
-		block.fillRect(x, y, width, height);
-		
 		Graphics2D g2d = (Graphics2D)block;
+		g2d.setColor(Color.BLACK);
+		g2d.fillRect(x, y, width, height);
 		g2d.rotate(angle);
 	}
 	public Rectangle getRect() {
@@ -70,9 +63,5 @@ public class Bar extends Component{
 	}
 	public int getX(){
 		return x;
-	}
-	
-	private void tilt(){
-		
 	}
 }
