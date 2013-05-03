cb871b4e9444e733f1f356dc986a516c353b3b0e
Retricide <eric.segev@gmail.com>
2013-05-03 08:07:11 -0400

    Added TOP variable to track top of rectangleSpace

--- Commons.java
+++ Commons.java
@@ -4,6 +4,7 @@ import java.awt.Toolkit;
 public interface Commons {
 	public static final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     public static final int WIDTH = screenSize.width;
-    public static final int HEIGHT = screenSize.height;
+    public static final int TOP = 100;
+    public static final int HEIGHT = screenSize.height-TOP;
     public static final int BOTTOM = HEIGHT;
 }
